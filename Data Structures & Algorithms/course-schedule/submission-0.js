ffb61f05e9f5fc2396b6dcd26a32924698df0de7
Nursultan Begaliev <97899879+begalievn@users.noreply.github.com>
2026-04-06 13:05:45 +0600

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        const map = new Map();
+
+        for (const [a, b] of prerequisites) {
+            map.set(a, b);
+        }
+
+        function dfs(a, b, visited) {
+            if (visited.has(b)) {
+                return false;
+            }
+
+            visited.add(a);
+
+            const prerequisiteB = map.get(b);
+            console.log('pre', prerequisiteB);
+            if (Number.isInteger(prerequisiteB)) {
+                return dfs(b, prerequisiteB, visited);
+            }
+
+            return true;
+        }
+
+        for (const [a, b] of prerequisites) {
+            const visited = new Set();
+            const possible = dfs(a, b, visited);
+            console.log('visited', visited);
+
+            if (!possible) return false;
+            if (possible && numCourses - 1 === visited.size) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
