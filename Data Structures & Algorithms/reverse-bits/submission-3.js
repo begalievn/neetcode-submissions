114d3b75e4a2b8002c733657259aa17a2f5217b2
Nursultan Begaliev <97899879+begalievn@users.noreply.github.com>
2026-04-14 12:29:27 +0600

    Add: reverse-bits - submission-3

---
+++ Data Structures & Algorithms/reverse-bits/submission-3.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number} - a positive integer
+     */
+    reverseBits(n) {
+        console.log('n', n);
+        let reversedBin = '';
+
+        while (n > 0) {
+            if (n & 1) {
+                reversedBin += '1';
+            } else {
+                reversedBin += '0';
+            }
+            n = n >> 1;
+        }
+
+        reversedBin += Array.from({ length: 32 - reversedBin.length }, () => '0').join('');
+        console.log('reversedBin', reversedBin);
+        console.log('length', reversedBin.length);
+
+        return parseInt(reversedBin, 2);
+    }
+}
