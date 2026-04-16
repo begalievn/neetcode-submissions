45e40f5a4da278c3569c3e7916619a7deb494adb
Nursultan Begaliev <97899879+begalievn@users.noreply.github.com>
2026-04-16 15:56:15 +0600

    Add: maximum-sum-circular-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-5.js
@@ -0,0 +1,57 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    // maxSubarraySumCircular(nums) {
+    //     let globMax = nums[0];
+    //     let globMin = nums[0];
+    //     let curMax = 0;
+    //     let curMin = 0;
+    //     let total = 0;
+
+    //     for(let num of nums) {
+    //         curMax = Math.max(curMax + num, num);
+    //         curMin = Math.min(curMax + num, num);
+    //         total += num;
+    //         globMax = Math.max(globMax, curMax);
+    //         globMin = Math.min(globMin, curMin);
+    //     }
+
+    //     return globMax > 0 ? Math.max(globMax, total - globMin) : globMax;
+
+    // }
+
+    maxSubarraySumCircular(nums) {
+        let globMax = nums[0],
+            globMin = nums[0];
+        let curMax = 0,
+            curMin = 0,
+            total = 0;
+
+        for (const num of nums) {
+            curMax = Math.max(curMax + num, num);
+            curMin = Math.min(curMin + num, num);
+            total += num;
+            globMax = Math.max(globMax, curMax);
+            globMin = Math.min(globMin, curMin);
+        }
+
+        return globMax > 0 ? Math.max(globMax, total - globMin) : globMax;
+    }
+
+    // maxSubarraySumCircular(nums) {
+    //     const n = nums.length;
+    //     let res = nums[0];
+
+    //     for (let i = 0; i < n; i++) {
+    //         let curSum = 0;
+    //         for (let j = i; j < i + n; j++) {
+    //             curSum += nums[j % n];
+    //             res = Math.max(res, curSum);
+    //         }
+    //     }
+
+    //     return res;
+    // }
+}
