b004ed500a6794c49e6cc3a91e21c8b3e5b5e09b
Nursultan Begaliev <97899879+begalievn@users.noreply.github.com>
2026-04-16 14:31:02 +0600

    Add: maximum-sum-circular-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-1.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubarraySumCircular(nums) {
+        const len = nums.length;
+        let maxSum = nums[0];
+        let curSum = 0;
+        let L = 0;
+        let R = 0;
+        const cache = new Map();
+
+        while (R < len * 2 && R - L !== len) {
+            if (cache.has(`${L},${R}`)) {
+                break;
+            }
+
+            if (curSum < 0) {
+                curSum = 0;
+                L = R;
+            }
+
+            curSum += nums[R % len];
+            if (curSum > maxSum) {
+                maxSum = curSum;
+            }
+
+            cache.set(`${L},${R}`, curSum);
+            R++;
+        }
+
+        console.log(cache);
+
+        return maxSum;
+
+    }
+}
