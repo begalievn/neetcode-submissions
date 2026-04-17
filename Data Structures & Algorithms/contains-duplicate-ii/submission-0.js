53518d9f3002ca5a331fd567a781149fbc6ace88
Nursultan Begaliev <97899879+begalievn@users.noreply.github.com>
2026-04-17 15:34:11 +0600

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    containsNearbyDuplicate(nums, k) {
+        let L = 0;
+        const set = new Set();
+
+        for (let R = 0; R < nums.length; R++) {
+            if (R - L > k) {
+                console.log('overlap', {
+                    L,
+                    R,
+                });
+                set.delete(nums[L]);
+                L++;
+            }
+            if (set.has(nums[R])) {
+                return true;
+            }
+            set.add(nums[R]);
+        }
+
+        console.log(set);
+
+        return false;
+    }
+}
