d404631c367e6dc7d326a32622c341fd8576e447
Nursultan Begaliev <97899879+begalievn@users.noreply.github.com>
2026-04-20 12:25:29 +0600

    Add: longest-turbulent-subarray - submission-0

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number}
+     */
+    maxTurbulenceSize(arr) {
+        let len = 0;
+        let last = 0;
+        let L = 0;
+
+        for (let R = 1; R < arr.length; R++) {
+            if (arr[R-1] < arr[R]) {
+                if (last !== -1) {
+                    L = R - 1;
+                }
+                len = Math.max(len, R - L + 1);
+                last = 1;
+            } else if (arr[R-1] > arr[R]) {
+                if (last !== 1) {
+                    L = R - 1;
+                } 
+                len = Math.max(len, R - L + 1);
+                last = -1;
+            } else {
+                last = 0;
+                L = R;
+            }
+        }
+
+        return len;
+    }
+}
