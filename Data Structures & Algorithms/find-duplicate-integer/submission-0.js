565c7351ffd5abefbf04d4c88c15976466c34af7
Nursultan Begaliev <97899879+begalievn@users.noreply.github.com>
2026-04-30 13:16:49 +0600

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        for (let i = 0; i < nums.length; i++) {
+            const index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                return nums[i];
+            }
+
+            nums[index] *= -1;
+        }
+
+        return -1;
+    }
+}
