d05c3d9ae9304723add45424f9d42ade0504da8b
Nursultan Begaliev <97899879+begalievn@users.noreply.github.com>
2026-05-01 23:24:12 +0600

    Add: intersection-of-two-arrays - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    intersection(nums1, nums2) {
+        const [short, long] = nums1.length < nums2.length ? [nums1, nums2] : [nums2, nums1];
+        const set = new Set();
+        const res = [];
+        for (const num of short) {
+            set.add(num);
+        }
+
+        for (const num of nums2) {
+            if (set.has(num)) {
+                res.push(num);
+                set.delete(num);
+            }
+        }
+
+        return res;
+    }
+}
