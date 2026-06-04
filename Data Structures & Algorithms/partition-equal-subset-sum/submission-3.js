e071101a09518fe02a570b16e607db2e0fc974aa
Nursultan Begaliev <97899879+begalievn@users.noreply.github.com>
2026-06-05 01:04:13 +0600

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canPartition(nums) {
+        const totalSum = nums.reduce((acc, num) => acc + num, 0);
+
+        if (totalSum % 2 == 1) return false;
+
+        const halfSum = totalSum / 2;
+        const n = nums.length;
+        this.memo = Array.from(Array(n + 1), () =>
+            Array(totalSum / 2 + 1).fill(null),
+        );
+
+        return this.dfs(nums, 0, halfSum);
+    }
+
+    dfs(nums, i, target) {
+        if (i === nums.length) {
+            return target === 0;
+        }
+        if (target < 0) {
+            return false;
+        }
+        if (this.memo[i][target] != null) {
+            return this.memo[i][target];
+        }
+
+        this.memo[i][target] =
+            this.dfs(nums, i + 1, target) ||
+            this.dfs(nums, i + 1, target - nums[i]);
+        return this.memo[i][target];
+    }
+}
