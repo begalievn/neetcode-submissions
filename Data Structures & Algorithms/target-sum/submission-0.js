c6bfba825ccdfe3249120a62172c73c49e87f501
Nursultan Begaliev <97899879+begalievn@users.noreply.github.com>
2026-06-05 12:44:00 +0600

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    findTargetSumWays(nums, target) {
+        const len = nums.length;
+        const cache = new Map();
+
+        function dfs(i, curSum) {
+            const key = `${i}`;
+
+            if (i >= len) {
+                if (curSum === target) return 1;
+                
+                return 0;
+            }
+
+            if (cache.has(key)) return cache.get(key);
+
+            const curNum = nums[i];
+            
+            const count = dfs(i + 1, curSum + curNum) + dfs(i + 1, curSum + (-1 * curNum));
+
+            cache.set(key, count);
+
+            return count;
+        }
+
+        return dfs(0, 0);
+    }
+}
